b8266b55fdce4b324236798615dd4d65be759d64
Lucas Oliveira <olivmath@protonmail.com>
2023-01-22 12:38:21 -0300

    add: function for check employee exists and only owner

--- src/AutoPay.Auth.sol
+++ src/AutoPay.Auth.sol
@@ -1,8 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import {Storage} from "./AutoPay.Storage.sol";
+import {Array, Storage} from "./AutoPay.Storage.sol";
+
+error EmployeeExists(address employee);
+error Unauthorized(address investor);
 
 abstract contract Auth is Storage {
+    using Array for address[];
+
     constructor() Storage() {}
+
+    function onlyOwner(address user) internal view {
+        if (user != owner) {
+            revert Unauthorized(user);
+        }
+    }
+
+    function checkEmployeeExists(address employee) internal view {
+        if (_employees.check(employee)) {
+            revert EmployeeExists(employee);
+        }
+    }
 }
