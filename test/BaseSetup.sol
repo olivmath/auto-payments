18b6ea6d9e6e19ff6dbe87a18d5ec7937be8d1d5
Lucas Oliveira <olivmath@protonmail.com>
2023-01-22 12:37:31 -0300

    add: tests for manage contrat

--- test/BaseSetup.sol
+++ test/BaseSetup.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+import {AutoPayments} from "../src/AutoPay.sol";
 import {Test} from "forge-std/Test.sol";
 import {Utils} from "./Utils.sol";
 
 contract BaseSetup is Test, Utils {
-    address owner;
-    address bob;
-    address alise;
+    AutoPayments ap;
+
     address[] users;
+    address owner;
+    address alise;
+    address bob;
 
     function setUp() public virtual {
         Utils utils = new Utils();
@@ -20,5 +23,8 @@ contract BaseSetup is Test, Utils {
         vm.label(bob, "Bob");
         alise = users[2];
         vm.label(alise, "Alise");
+
+        vm.prank(owner);
+        ap = new AutoPayments();
     }
 }
--- test/ManageEmployees.t.sol
+++ test/ManageEmployees.t.sol
@@ -1,11 +1,59 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
+import {EmployeeExists} from "../src/AutoPay.Auth.sol";
+import {AutoPayments} from "../src/AutoPay.sol";
 import {BaseSetup} from "./BaseSetup.sol";
 
 contract ManageTest is BaseSetup {
+    uint256 salaryAmount;
 
-    function setUp() public virtual override{
+    function setUp() public virtual override {
         BaseSetup.setUp();
+        salaryAmount = 100;
+    }
+
+    function test_that_an_employee_can_be_added_to_the_contract() public {
+        vm.prank(owner);
+        ap.addEmployee(bob, salaryAmount);
+        address[] memory employees = ap.employees();
+
+        assertEq(employees.length, 1, "Employee not added");
+        assertEq(employees[0], bob, "Wrong employee added");
+        assertEq(ap.nextPayment(bob), 172801, "Payment not added");
+    }
+
+    function test_that_an_employee_cannot_be_added_twice() public {
+        vm.startPrank(owner);
+
+        ap.addEmployee(bob, salaryAmount);
+        vm.expectRevert(abi.encodeWithSelector(EmployeeExists.selector, bob));
+        ap.addEmployee(bob, salaryAmount);
+
+        vm.stopPrank();
+    }
+
+    function test_that_an_employee_can_be_removed() public {
+        vm.startPrank(owner);
+
+        ap.addEmployee(bob, salaryAmount);
+        ap.removeEmployee(bob);
+
+        vm.stopPrank();
+        address[] memory employees = ap.employees();
+
+        assertEq(employees.length, 0, "Employee not removed");
+    }
+
+    function test_that_an_employee_can_have_their_salary_edited() public {
+        vm.startPrank(owner);
+
+        ap.addEmployee(bob, salaryAmount);
+        uint256 newSalary = 200;
+        ap.editEmployee(bob, newSalary);
+
+        vm.stopPrank();
+
+        assertEq(ap.salaryOf(bob), newSalary * 10e17, "Salary not edited");
     }
 }
