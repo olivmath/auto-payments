512cf7ab7797ae56e604bf7ce68e305ab661812f
Lucas Oliveira <olivmath@protonmail.com>
2023-01-22 17:11:48 -0300

    add: test to manage employees

--- test/AutoPay/ManageEmployees.t.sol
+++ test/AutoPay/ManageEmployees.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import {EmployeeExists, Unauthorized} from "../../src/AutoPay.Auth.sol";
+import {EmployeeNotExists, EmployeeExists, Unauthorized} from "../../src/AutoPay.Auth.sol";
 import {AutoPayments} from "../../src/AutoPay.sol";
 import {BaseSetup} from "../BaseSetup.sol";
 
@@ -10,40 +10,52 @@ contract ManageTest is BaseSetup {
 
     function setUp() public virtual override {
         BaseSetup.setUp();
-        salaryAmount = 100;
     }
 
     function test_that_an_unauthorized_user_try_added_new_employee() public {
         vm.prank(bob);
         vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector, bob));
-        ap.addEmployee(bob, 10000);
+        ap.addEmployee(bob, 1);
     }
 
-    function test_that_an_employee_can_be_added_to_the_contract() public {
+    function test_that_an_employee_can_be_added_to_the_contract_with_fix_salary(address newEmployee) public {
+        uint256 salary = 12;
         vm.prank(owner);
-        ap.addEmployee(bob, salaryAmount);
+        ap.addEmployee(newEmployee, salary);
         address[] memory employees = ap.employees();
 
         assertEq(employees.length, 1, "Employee not added");
-        assertEq(employees[0], bob, "Wrong employee added");
-        assertEq(ap.nextPayment(bob), 172801, "Payment not added");
+        assertEq(employees[0], newEmployee, "Wrong employee added");
+        assertEq(ap.salaryOf(newEmployee), 12, "Wrong salary added for employee");
+        assertEq(ap.nextPayment(newEmployee), 172801, "Next payment not added");
+    }
+
+    function test_that_an_employee_can_be_added_to_the_contract(address newEmployee, uint256 salary) public {
+        vm.prank(owner);
+        ap.addEmployee(newEmployee, salary);
+        address[] memory employees = ap.employees();
+
+        assertEq(employees.length, 1, "Employee not added");
+        assertEq(employees[0], newEmployee, "Wrong employee added");
+        assertEq(ap.salaryOf(newEmployee), salary, "Wrong salary added for employee");
+        assertEq(ap.nextPayment(newEmployee), 172801, "Next payment not added");
     }
 
     function test_that_an_employee_cannot_be_added_twice() public {
         vm.startPrank(owner);
 
-        ap.addEmployee(bob, salaryAmount);
-        vm.expectRevert(abi.encodeWithSelector(EmployeeExists.selector, bob));
-        ap.addEmployee(bob, salaryAmount);
+        ap.addEmployee(bob, 100);
+        vm.expectRevert(abi.encodeWithSelector(EmployeeNotExists.selector, bob));
+        ap.addEmployee(bob, 100);
 
         vm.stopPrank();
     }
 
-    function test_that_an_employee_can_be_removed() public {
+    function test_that_an_employee_can_be_removed(address newEmployee, uint256 salary) public {
         vm.startPrank(owner);
 
-        ap.addEmployee(bob, salaryAmount);
-        ap.removeEmployee(bob);
+        ap.addEmployee(newEmployee, salary);
+        ap.removeEmployee(newEmployee);
 
         vm.stopPrank();
         address[] memory employees = ap.employees();
@@ -51,15 +63,30 @@ contract ManageTest is BaseSetup {
         assertEq(employees.length, 0, "Employee not removed");
     }
 
-    function test_that_an_employee_can_have_their_salary_edited() public {
+    function test_that_an_employee_can_have_their_salary_edited(address newEmployee, uint256 salary) public {
+        vm.assume(0 < salary && salary < 1000);
         vm.startPrank(owner);
 
-        ap.addEmployee(bob, salaryAmount);
-        uint256 newSalary = 200;
-        ap.editEmployee(bob, newSalary);
+        ap.addEmployee(newEmployee, salary);
+
+        address[] memory employees = ap.employees();
+
+        assertEq(employees[0], newEmployee, "Wrong employee added");
+        assertEq(ap.salaryOf(newEmployee), salary, "Wrong salary added for employee");
+
+        ap.editEmployee(newEmployee, salary * 2);
+        assertEq(ap.salaryOf(newEmployee), salary * 2, "Salary not edited");
 
         vm.stopPrank();
+    }
 
-        assertEq(ap.salaryOf(bob), newSalary * 10e17, "Salary not edited");
+    function test_that_non_existent_employee_cannot_be_edited(address nonExistentEmployee) public {
+        vm.startPrank(owner);
+        address[] memory employees = ap.employees();
+
+
+        assertEq(employees.length, 0, "Employee have more than one");
+        vm.expectRevert(abi.encodeWithSelector(EmployeeExists.selector, nonExistentEmployee));
+        ap.editEmployee(nonExistentEmployee, 200);
     }
 }
