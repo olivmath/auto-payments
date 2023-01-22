e9dc35bf077e11af43000cff9219b711cc4fabab
Lucas Oliveira <olivmath@protonmail.com>
2023-01-22 17:12:02 -0300

    remove: unused code

--- src/AutoPay.Events.sol
+++ src/AutoPay.Events.sol
@@ -2,8 +2,5 @@
 pragma solidity ^0.8.17;
 
 abstract contract Events {
-    event NewEmployee(address employee, uint256 salary, uint256 nextPayment);
-    event NewSalary(address employee, uint256 newSalary);
-    event EmployeeRemoved(address employee);
     event Payed(address employee, uint256 salary);
 }
