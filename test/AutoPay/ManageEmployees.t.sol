d0caa25a3052977c95a1250b43e67d6639dffa0e
Lucas Oliveira <olivmath@protonmail.com>
2023-01-22 23:50:33 -0300

    update: license

--- test/AutoPay/ManageEmployees.t.sol
+++ test/AutoPay/ManageEmployees.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 import {EmployeeNotExists, EmployeeExists, Unauthorized} from "../../src/AutoPay.Auth.sol";
